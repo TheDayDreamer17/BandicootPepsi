c7481847737dc66124dda25c9006769f6adbfbaa
Vismay thaker <vismaythaker008@gmail.com>
2022-02-28 00:29:02 +0530

    c

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build new.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/b2.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
